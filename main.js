4d86e8d5a230f4fc7d1f4547b0da66353df8a2de
Michael David <lordkay07@gmail.com>
2022-04-12 13:56:31 +0100

    Layout Build

---
+++ main.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+
+// let inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", () => {
+  myLeads.push("www.awesomelead.com");
+  console.log(myLeads);
+});
