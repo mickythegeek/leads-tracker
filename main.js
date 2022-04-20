718edb59f0824f0c9a7db5367b6fa353a8c9a087
Michael David <lordkay07@gmail.com>
2022-04-20 09:39:01 +0100

    Using Template Strings and Styling the Saved Leads

--- main.js
+++ main.js
@@ -9,15 +9,18 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  renderLeads();
   inputEl.value = "";
+  renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
-    console.log(listItems);
+    listItems += `<li>
+              <a target = '_blank' href = '${myLeads[i]}'>
+                ${myLeads[i]} 
+              </a>
+          </li>`;
   }
   ulEl.innerHTML = listItems;
 }
