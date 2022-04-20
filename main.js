1f0dda1acf7d85f438b6f94ed5ca66394266e3de
Michael David <lordkay07@gmail.com>
2022-04-20 13:24:29 +0100

    Implementing the Save Tab Button

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
 
 // Get the leads from LocalStorage
@@ -13,6 +14,7 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+const tabs = [{ url: "https://www.mickythegek.com" }];
 
 function render(leads) {
   let listItems = "";
@@ -26,7 +28,15 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-// When the Save Input button is clicked
+// Save Tab
+tabBtn.addEventListener("click", () => {
+  // Using localStorage
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+// The Save Input
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   inputEl.value = "";
@@ -35,6 +45,7 @@ inputBtn.addEventListener("click", () => {
   render(myLeads);
 });
 
+// The Delete Button
 deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
